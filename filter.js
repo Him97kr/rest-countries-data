13882218a53dbee8e8a8eb0fd566ce3587de7fc7
Him97kr <himanshu97kumar@gmail.com>
2026-06-12 21:49:32 +0530

    updated data

--- filter.js
+++ filter.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const fs = require('fs');
 
 const API_KEY = process.env.REST_COUNTRIES_API_KEY;
-const BASE_URL = 'https://api.restcountries.com/countries/v5?response_fields=names.common,population,area,capitals.name,flag.emoji,flag.url_png,flag.url_svg,codes.alpha_2,codes.alpha_3,languages.name,region,currencies,';
+const BASE_URL = 'https://api.restcountries.com/countries/v5?response_fields=names.common,names.official,population,area,capitals.name,flag.emoji,flag.url_png,flag.url_svg,codes.alpha_2,codes.alpha_3,languages.name,region,currencies,';
 
 async function fetchCountries() {
     try {
